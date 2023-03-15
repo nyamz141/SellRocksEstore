332b443bf3573bca75f80d1b65be789219c60c86
nyamz141 <51674315+nyamz141@users.noreply.github.com>
2023-03-15 17:10:23 +1000

    added User Accounts APi skeleton

---
+++ UserAccounts.API/Controllers/UserAccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserAccounts.API.Controllers
+{
+    public class UserAccountsController: ControllerBase
+    {
+        public UserAccountsController()
+        {
+                
+        }
+    }
+}
